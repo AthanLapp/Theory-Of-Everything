ab40fac5c0352d5750a4aaa96ebce94552291598
Αθανάσιος Λάππας (Athanasios Lappas) <Athan.Lapp@gmail.com>
2022-08-27 14:19:07 +0300

    Use scroll

---
+++ Turtle_Graphics_Cir le_To_Line_And_Expansion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+turtl1 = turtle.Turtle()
+screen = turtle.Screen()
+turtl1.speed(10)
+turtle.screensize(1000000,1000000)
+for i in range(1,1000000):
+    turtl1.forward(i)
+    turtl1.left(i)
+
