c787aa7bf35927576523a6fb07b16e116003b125
Αθανάσιος Λάππας (Athanasios Lappas) <Athan.Lapp@gmail.com>
2022-08-27 16:30:45 +0300

    Add files via upload

--- Turtle_Graphics_Cir le_To_Line_And_Expansion.py
+++ Turtle_Graphics_Cir le_To_Line_And_Expansion.py
@@ -10,8 +10,8 @@ turtl1 = turtle.Turtle()
 screen = turtle.Screen()
 turtl1.speed(10)
 turtle.screensize(1000000,1000000)
-for i in range(1,1000000):
+for i in range(1,10000000):
     turtl1.forward(i)
     turtl1.left(i)
-screen.getshapes()
+print(screen.getshapes())
 
